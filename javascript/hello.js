f63f29b8f74588ef11217e4ae2616d51bfca5493
Rajangithub <rajanyadav8119@gmail.com>
2025-03-29 16:21:59 +0530

    starting js with making some factory or feature

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,3 +1,22 @@
 // print statement
 
 console.log("hello js")
+
+function printChai(){
+    console.log(`hello chai`)
+}
+
+function bringBrush(numberOfBrush){
+    console.log(`hanji, le aaya ${numberOfBrush} brush`)
+
+}
+
+function addTwo(num1, num2){
+    return num1+num2
+}
+
+bringBrush(12)
+printChai()
+addTwo(25,21)
+
+console.log(addTwo(25,50))
